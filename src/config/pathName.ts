1db9fcc84cf17f955e16b15a3aebc67369293ecf
Rohit Singh <singhrohit81111@gmail.com>
2024-09-13 10:52:03 +0530

    feat: handled routes

---
+++ src/config/pathName.ts
@@ -0,0 +1,3 @@
+export const CONATCTS='/';
+export const CHARTS_AND_MAPS='/charts-and-maps';
+// export const CONATCTS=''
--- src/routes/appRoutes.tsx
+++ src/routes/appRoutes.tsx
@@ -1,10 +1,25 @@
 import { createBrowserRouter } from "react-router-dom";
+import BaseLayout from '../components/layouts/BaseLayout'
 import Contacts from "../pages/Contacts";
 import ChartsAndMaps from "../pages/ChartsAndMaps";
+import { CHARTS_AND_MAPS, CONATCTS } from "../config/pathName";
 
 const routes = createBrowserRouter([
-  { path: "/contacts", element: <Contacts /> },
-  { path: "charts-and-maps", element: <ChartsAndMaps/> },
+ {
+  path:'',
+  element:<BaseLayout/>,
+  children:[
+    {
+      index:true,
+      path:CONATCTS,
+      element:<Contacts/>
+    },
+    {
+      path:CHARTS_AND_MAPS,
+      element:<ChartsAndMaps/>
+    }
+  ]
+ }
 ]);
 
 export { routes };
